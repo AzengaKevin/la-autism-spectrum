18e6aa38cffd69d18e0129d5c0a195856da74052
Azenga Kevin <azenga.kevin7@gmail.com>
2020-12-15 16:03:48 +0300

    Wip, commence questionnaire section and hookup everything

---
+++ database/factories/QuestionnaireFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use App\Models\Questionnaire;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QuestionnaireFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Questionnaire::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'min_age' => $this->faker->numberBetween(1, 100)
+        ];
+    }
+}
---
+++ tests/Feature/QuestionnaireCrudTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Questionnaire;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class QuestionnaireCrudTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @group questionnaire */
+    public function test_questionnaire_can_be_created()
+    {
+        $questionnaireData = Questionnaire::factory()->make()->toArray();
+
+        $questionnaire = Questionnaire::create($questionnaireData);
+
+        $this->assertCount(1, Questionnaire::all());
+
+        $this->assertEquals($questionnaireData['title'], $questionnaire->title);
+        $this->assertEquals($questionnaireData['description'], $questionnaire->description);
+        $this->assertEquals($questionnaireData['user_id'], $questionnaire->user_id);
+        $this->assertEquals($questionnaireData['min_age'], $questionnaire->min_age);
+    }
+
+    /** @group questionnaires */
+    public function test_a_questionnaire_can_be_read()
+    {
+        Questionnaire::factory()->create();
+
+        $questionnaire = Questionnaire::find(1);
+
+        $this->assertNotNull($questionnaire);
+    }
+
+    /** @group questionnaire */
+    public function test_a_questionnaire_can_be_updated()
+    {
+        Questionnaire::factory()->create();
+
+        $questionnaireData = Questionnaire::factory()->make()->toArray();
+
+        $questionnaire = Questionnaire::find(1);
+
+        $this->assertNotNull($questionnaire);
+
+        $questionnaire->update($questionnaireData);
+
+        $this->assertEquals($questionnaireData['title'], $questionnaire->title);
+        $this->assertEquals($questionnaireData['description'], $questionnaire->description);
+        $this->assertEquals($questionnaireData['user_id'], $questionnaire->user_id);
+        $this->assertEquals($questionnaireData['min_age'], $questionnaire->min_age);
+        
+    }
+
+    /** @group questionnaires */
+    public function test_a_questionnaire_can_be_deleted()
+    {
+        Questionnaire::factory()->create();
+
+        $questionnaire = Questionnaire::find(1);
+        $questionnaire->delete();
+
+        $this->assertCount(0, Questionnaire::all());
+
+        $this->assertCount(1, Questionnaire::withTrashed()->get());
+        
+    }
+}
