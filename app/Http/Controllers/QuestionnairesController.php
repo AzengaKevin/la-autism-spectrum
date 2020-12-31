8e0e1e0128c22a8c0d09cf4638d5ac50eced115f
Azenga Kevin <azenga.kevin7@gmail.com>
2020-12-31 16:40:37 +0300

    Wip, remove unnecessary and delegate the storage funtionality to questionnaire screeing controller

--- app/Http/Controllers/QuestionnairesController.php
+++ app/Http/Controllers/QuestionnairesController.php
@@ -17,48 +17,4 @@ class QuestionnairesController extends Controller
         return view('questionnaires.index', compact('questionnaires'));
     }
 
-    /**
-     * Shows a single questionnaire and a from for taking the questionnaire
-     */
-    public function show(Questionnaire $questionnaire)
-    {
-        
-        $questionnaire->load('questions.answers');
-
-        return view('questionnaires.show', compact('questionnaire'));
-        
-    }
-
-    /**
-     * Stores a user response to the database
-     */
-    public function store(Request $request, Questionnaire $questionnaire)
-    {
-
-        $data = $request->validate([
-            'responses' => ['bail', 'required', 'array', 'min:1'],
-            'responses.*.question_id' => ['bail', 'required', 'numeric'],
-            'responses.*.answer_id' => ['bail', 'required', 'numeric'],
-            'respondent' => ['bail', 'array'],
-        ]);
-        
-        $screeningData = [];
-
-        if(isset($data['respondent'])){
-            $screeningData['name'] = $data['respondent']['name'];
-            $screeningData['email'] = $data['respondent']['email'];
-        }else{
-            if(Auth::check()){
-                $screeningData['user_id'] = Auth::id();
-            }
-        }
-
-        $screening = $questionnaire->screenings()->create($screeningData);
-
-        $screening->responses()->createMany($data['responses']);
-
-        return redirect()->route('questionnaires.index');
-
-    }
-
 }
